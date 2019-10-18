75643d34ab06e80d310153f022b69f9f3ed6e290
Maxym_Ko <koval.maxim@unicyb.kiev.ua>
2019-10-18 14:05:09 +0300

    18.10

--- 1_course/1_semester/homeworks/7.py
+++ 1_course/1_semester/homeworks/7.py
@@ -10,8 +10,7 @@ def is_perpendicular_lines(a1: float, b1: float, a2: float, b2: float) -> bool:
 
 # 7_25b
 def is_collinear_lines(a1: float, b1: float, a2: float, b2: float) -> bool:
-    # pre: a2 != 0 and b2 != 0
-    if a1 / a2 == b1 / b2:
+    if a1 * b2 == a2 * b1:
         return True
     return False
 
--- 1_course/1_semester/labs/lab_1.py
+++ 1_course/1_semester/labs/lab_1.py
@@ -5,26 +5,17 @@ def info_author():
     about_author = f"\
     Author: Koval\n\
             Maxym\n\
-    Group: К-12\n"
+    Group:  К-12\n"
     print(about_author)
 
 
-def is_number(s: str) -> bool:
-    """Check if s can be represented as a number"""
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
-
-
-def domain(x: float) -> float:
+def domain(x: float):
     """Check if x is in domain of an expression"""
-    return x - 5 and x + 6 and x + 12
+    return (x - 5) and (x + 6) and (x + 12)
 
 
-def f(x: float) -> float: # Чи треба писати, який вираз вираховується?
-    """Calculate the next expression: cos(29/63) - 22e/51pi * 10 / ((x - 5)(x - 6)) + 7sin(x - 8) - 5 / (x + 12)"""
+def f(x: float) -> float:  # Чи треба писати, який вираз вираховується?
+    """Calculate the next expression: cos(29/63) - 22e/(51pi) * 10 / ((x - 5)(x - 6)) + 7sin(x - 8) - 5 / (x + 12)"""
     return math.cos(29 / 63) - 22 * math.e / 51 / math.pi * 10 / (x - 5) / (x + 6) + 7 * math.sin(x - 8) - 5 / (x + 12)
 
 
@@ -39,17 +30,18 @@ def f_total(x: float) -> bool and float:
         return False, -1
 
 
-def run():
-    print('Variant 71 \nThis program calculate an expression using variables, entered by the user')
+def main():
+    print('Variant #71 \nThis program calculate an expression using variables, entered by the user')
     info_author()
 
     x = input('Input a value of x: ')
 
-    if not is_number(x): # Якшо тут повернути -1, і продовжувати програму не в else?
+    try:
+        x = float(x)
+    except ValueError:
         print('\nwrong input \nYou have input a value of x that can\'t be represented as a number. Please, restart program and try again.')
     else:
         print('\n***** do calculations ... ', end='')
-        x = float(x)
         success, result = f_total(x)
         print('done\n')
 
@@ -60,4 +52,4 @@ def run():
             print(f'result = undefined \n(result can\'t be calculated because x = {x} isn\'t in domain of a function.)')
 
 
-run()
+main()
