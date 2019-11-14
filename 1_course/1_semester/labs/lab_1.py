76680123590b688dab18eb872625372392bdb05f
Maxym_Ko <koval.maxim@unicyb.kiev.ua>
2019-11-14 22:13:04 +0200

    14.11

--- 1_course/1_semester/labs/lab_1.py
+++ 1_course/1_semester/labs/lab_1.py
@@ -10,19 +10,19 @@ def info_author():
 
 
 def domain(x: float):
-    """Check if x is in domain of an expression"""
+    """Check if x is in the domain of the expression"""
     return (x - 5) and (x + 6) and (x + 12)
 
 
-def f(x: float) -> float:  # Добавити формулу в докуаментацію
-    """Calculate the expression"""
+def f(x: float) -> float:
+    """Calculate the value of the expression"""
     return math.cos(29 / 63) - 22 * math.e / 51 / math.pi * 10 / (x - 5) / (x + 6) + 7 * math.sin(x - 8) - 5 / (x + 12)
 
 
 def f_total(x: float) -> bool and float:
     """
-    Calculate expression if it is possible
-    Return True if calculation is successful or False if not and result of calculation
+    Calculate the value of the expression if it is possible
+    Return True if calculation is successful or False if not and the result of the calculation
     """
     if not domain(x):
         return False, None
@@ -31,25 +31,28 @@ def f_total(x: float) -> bool and float:
 
 
 def main():
-    print('Variant #71 \nThis program calculate an expression using variables, entered by the user')
+    print("Variant #71 \nThis program calculate an expression using variables, entered by the user")
     info_author()
 
-    x = input('Input a value of x: ')
+    x = input('Input the value of x: ')
 
     try:
         x = float(x)
     except ValueError:
-        print('\nwrong input \nYou have input a value of x that can\'t be represented as a number. Please, restart program and try again.')
+        print("\nwrong input \nYou have input a value of x that can't be represented as a number. Please, "
+              "restart program and try again.")
     else:
-        print('\n***** do calculations ... ', end='')
+        print("\n***** do calculations ... ", end="")
         success, result = f_total(x)
         print('done\n')
 
-        print(f'for x = {x:.6f}')
+        print(f"for x = {x:.6f}")
         if success:
-            print(f'result = {result:.6f}')
+            print(f"result = {result:.6f}")
         else:
-            print(f'result = undefined \n(result can\'t be calculated because x = {x} isn\'t in domain of a function.)')
+            print(
+                f"result = undefined \nThe result can\'t be calculated because x = {x} isn't in the domain of the "
+                "expression.")
 
 
 main()
--- 1_course/1_semester/lessons/test.py
+++ 1_course/1_semester/lessons/test.py
@@ -1,16 +1,9 @@
+import math
+
+
 def func(x):
-    res = None
-    if x <= 5:
-        res = 0
-    elif x < 3:
-        res = 2 * x
-    elif x <= 7:
-        res = x + 2
-    else:
-        res = x
-    return res
+    return 0.25 * math.log((1 + x) / (1 - x)) + 0.5 * math.atan(x)
 
 
-print(float('inf'))
-print(float('-inf'))
-print(float('nan'))
+x = float(input('... '))
+print(func(x))
